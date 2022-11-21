d61a9d54a13878654ce13df9506d8525e25cffef
Artem Baikov <artembaikov@Artems-MacBook-Pro.local>
2022-11-21 20:12:43 +0300

    Implemented rating function for fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,30 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        array
+          .select { |film| valid_rating?(film) }
+          .select { |film| enough_countries?(film) }
+          .reduce({ rating: 0, amount: 0 }) { |info, film| aggregate_film_rating(film, info) }
+          .then { |info| info[:rating] / info[:amount] }
       end
 
       def chars_count(_films, _threshold)
         0
+      end
+
+      def valid_rating?(film)
+        !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive?
+      end
+
+      def enough_countries?(film)
+        !film['country'].nil? && film['country'].split(',').length >= 2
+      end
+
+      def aggregate_film_rating(film, info)
+        info[:rating] += film['rating_kinopoisk'].to_f
+        info[:amount] += 1
+        info
       end
     end
   end
