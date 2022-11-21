537b3b6ffceca9fcfeaf0467dd4574728a708d38
Artem Baikov <artembaikov@Artems-MacBook-Pro.local>
2022-11-21 17:06:01 +0300

    Implemented search function for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,8 +6,27 @@ module Exercise
         array.map { |element| element.positive? ? max : element }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        binary_search(array, 0, array.length - 1, query)
+      end
+
+      private
+
+      def binary_search(array, first_index, last_index, query)
+        if first_index < last_index
+          middle_index = (first_index + last_index) / 2
+          if query > array[middle_index]
+            binary_search(array, middle_index + 1, last_index, query)
+          elsif query < array[middle_index]
+            binary_search(array, first_index, middle_index - 1, query)
+          else
+            middle_index
+          end
+        elsif first_index == last_index
+          array[first_index] == query ? first_index : -1
+        else
+          -1
+        end
       end
     end
   end
